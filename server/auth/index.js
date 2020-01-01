2eed80dcdbb4a95bd268a34968e7097adb525d50
Ivan Marchuk <ivan.marchuk@mail.ru>
2020-01-02 01:58:38 +0300

    finished basic sign up

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const Joi = require('joi')
+const bcrypt = require('bcryptjs')
 
-const db = require('../db/connection.js')
+const db = require('../db/connection')
+const users = db.get('users')
+users.createIndex('username', { unique: true})
 
 const router = express.Router()
 
@@ -16,14 +19,42 @@ router.get('/', (req, res) => {
 // POST auth/signup
 const schema = Joi.object().keys({
     username: Joi.string().regex(/(^[a-zA-Z0-9_]*$)/).min(3).max(30).required(), 
-    password: Joi.string().regex(/^[a-zA-Z0-9]{8,30}$/).required(),
-    email: Joi.string().email()
+    email: Joi.string().email(),
+    password: Joi.string().regex(/^[a-zA-Z0-9]{8,30}$/).required()
 })
 
-router.post('/signup', (req, res) => {
+router.post('/signup', (req, res, next) => {
     const result = Joi.validate(req.body, schema)
 
-    res.json(result)
+    if (result.error == null) {
+        // unique username check
+        users.findOne({
+            username: req.body.username
+        })
+        .then(user => {
+            // there is user -> err
+            if (user){
+                const error = new Error('That username is already taken')
+                next(error)
+            } else {
+                // hash password and register
+                bcrypt.hash(req.body.password, 12).then(hashedPassword => {
+                    const newUser = {
+                        username: req.body.username,
+                        password: hashedPassword,
+                        email: req.body.email
+                    }
+
+                    users.insert(newUser).then(insertedUser => {
+                        res.json(insertedUser)
+                    })
+                })
+            }
+        })
+    } else {
+        // goes to the errorhandler
+        next(result.error) 
+    }
 })
 
 module.exports = router
