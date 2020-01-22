f612f2aba5c4d756ea9018c336c05eb4d2f6e600
Ivan Marchuk <ivan.marchuk@mail.ru>
2020-01-22 19:17:06 +0300

    added create admin task serverside

--- server/tasks/createAdminUser.js
+++ server/tasks/createAdminUser.js
@@ -3,41 +3,17 @@ const db = require('../db/connection')
 const users = db.get('users')
 require('dotenv').config()
 
-'use strict';
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', _ => {
-    inputString = inputString.trim().split('\n').map(string => {
-        return string.trim();
-    });
-    
-    main();    
-});
-
-function readline() {
-    return inputString[currentLine++];
-}
-
+const rl = require('readline-sync')
 
 async function createAdminUser() {
-    console.log('Enter your username: ')
-    const input_username = readline()
-    console.log('Enter your email: ')
-    const input_email = readline()
-
     try {
+
+        var input_username = rl.question('Enter your username: ')
+        var input_email = rl.question('Enter your email: ')
+
         const user = await users.findOne({ role: 'admin' }) 
         if (!user) {
-            await users.create({
+            await users.insert({
                 username: input_username,
                 password: await bcrypt.hash(process.env.DEFAULT_ADMIN_PASSWORD, 12),
                 email: input_email,
@@ -45,9 +21,13 @@ async function createAdminUser() {
                 active: true
             })
             console.log('Admin User Created!')
+        } else {
+            console.log('Admin User already exists :(')
         }
     } catch (error) {
         console.error(error)
+    } finally {
+        db.close()
     }
 } 
 
