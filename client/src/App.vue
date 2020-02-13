2765d4e7875190ca238ca511792397978316592f
Ivan Marchuk <ivan.marchuk@mail.ru>
2020-02-13 23:19:23 +0300

    changing to vuetify

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,44 +1,46 @@
 <template>
   <div id="app">
-    <nav class="blue-grey darken-3">
-      <div class="nav-wrapper">
-        <a href="#!" class="brand-logo">Logo</a>
-        <a href="#" data-target="mobile-demo" class="sidenav-trigger">
-          <i class="material-icons">menu</i>
-        </a>
-        <ul class="right hide-on-med-and-down">
-          <li>
-            <a href="sass.html">Sass</a>
-          </li>
-          <li>
-            <a href="badges.html">Components</a>
-          </li>
-          <li>
-            <a href="collapsible.html">Javascript</a>
-          </li>
-          <li>
-            <a href="mobile.html">Mobile</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <v-app>
+      <nav class="blue-grey darken-3">
+        <div class="nav-wrapper">
+          <a href="#!" class="brand-logo">Logo</a>
+          <a href="#" data-target="mobile-demo" class="sidenav-trigger">
+            <i class="material-icons">menu</i>
+          </a>
+          <ul class="right hide-on-med-and-down">
+            <li>
+              <a href="sass.html">Sass</a>
+            </li>
+            <li>
+              <a href="badges.html">Components</a>
+            </li>
+            <li>
+              <a href="collapsible.html">Javascript</a>
+            </li>
+            <li>
+              <a href="mobile.html">Mobile</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
 
-    <ul class="sidenav" id="mobile-demo">
-      <li id="inner">
-        <a href="sass.html">Sass</a>
-      </li>
-      <li id="inner">
-        <a href="badges.html">Components</a>
-      </li>
-      <li id="inner">
-        <a href="collapsible.html">Javascript</a>
-      </li>
-      <li id="inner">
-        <a href="mobile.html">Mobile</a>
-      </li>
-    </ul>
+      <ul class="sidenav" id="mobile-demo">
+        <li id="inner">
+          <a href="sass.html">Sass</a>
+        </li>
+        <li id="inner">
+          <a href="badges.html">Components</a>
+        </li>
+        <li id="inner">
+          <a href="collapsible.html">Javascript</a>
+        </li>
+        <li id="inner">
+          <a href="mobile.html">Mobile</a>
+        </li>
+      </ul>
 
-    <router-view class="container pt-4" />
+      <router-view class="container pt-4" />
+    </v-app>
   </div>
 </template>
 
