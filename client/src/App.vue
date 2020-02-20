e44d42b0b2b23f1c44600115859b4096761d9369
Ivan Marchuk <ivan.marchuk@mail.ru>
2020-02-20 20:52:55 +0300

    added side-navbar navigation

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,7 +3,34 @@
     <v-app>
       <v-navigation-drawer v-model="drawer" app clipped>
         <v-list dense>
-          <v-list-item>
+          <v-list-item @click="$router.push({name: 'home'})">
+            <v-list-item-action>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>Home</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item @click="$router.push({name: 'signup'})">
+            <v-list-item-action>
+              <v-icon>mdi-checkbox-marked-circle</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>Sign Up</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item @click="$router.push({name: 'login'})">
+            <v-list-item-action>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>Login</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item @click="$router.push({name: 'dashboard'})">
             <v-list-item-action>
               <v-icon>mdi-widgets</v-icon>
             </v-list-item-action>
@@ -11,20 +38,12 @@
               <v-list-item-title>Dashboard</v-list-item-title>
             </v-list-item-content>
           </v-list-item>
-          <v-list-item>
-            <v-list-item-action>
-              <v-icon>mdi-wrench</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title>Settings</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
         </v-list>
       </v-navigation-drawer>
 
       <v-app-bar app clipped-left>
         <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-        <v-toolbar-title>Application</v-toolbar-title>
+        <v-toolbar-title>Find Your Music</v-toolbar-title>
       </v-app-bar>
       <router-view class=".d-flex" />
     </v-app>
